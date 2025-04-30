bad6e32c6e34efbc3c219be57f7b897fa94c73f1
Kazufumi Ohkawa <kerolinq@gmail.com>
2025-04-30 15:05:49 +0900

    Add Atom class

--- FMOkit/Atom.py
+++ FMOkit/Atom.py
@@ -1,3 +1,29 @@
 class Atom:
-    def __init__(self):
-        print("Atom initialized")
+    def __init__(self, **kwargs):
+        """
+        Initialize an Atom object with the given parameters.
+        :param kwargs: Dictionary containing the parameters for the Atom.
+            - id (int): The ID of the atom.
+            - type_symbol (str): The type symbol of the atom.
+            - atom_id (str): The ID of the atom in the system.
+            - charge (float): The charge of the atom.
+            - x (float): The x-coordinate of the atom.
+            - y (float): The y-coordinate of the atom.
+            - z (float): The z-coordinate of the atom.
+            - atom_id (int): The ID of the atom in the system.
+        """
+        self.id = kwargs["id"]
+        self.type_symbol = kwargs["type_symbol"]
+        self.atom_id = kwargs["atom_id"]
+        self.charge = kwargs["charge"]
+        self.x = kwargs["x"]
+        self.y = kwargs["y"]
+        self.z = kwargs["z"]
+        self.atom_id = kwargs["atom_id"]
+
+    def __repr__(self):
+        return f"<{self.id:3d} ({self.atom_id:<3}) {self.x:7.3f} {self.y:7.3f} {self.z:7.3f}>"
+
+    @property
+    def fmoxyz(self):
+        return f"{self.id:>7d} {self.type_symbol:>6}    {self.x:>18.8f}{self.y:>18.8f}{self.z:>18.8f}"
