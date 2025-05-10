8e66fbfd221c045e0400bdc02e85e9d6a6e45c46
Kazufumi Ohkawa <kerolinq@gmail.com>
2025-05-10 22:29:42 +0900

    Add cli fix #2

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FMOkit',
+    version='0.1',
+    py_modules=['FMOkit'],
+    install_requires=[
+        'click',
+        'gemmi',
+        'rdkit',
+        'pdbfixer',
+        'mdtraj',
+        'openmm',
+        'openmmforcefields',
+        'openff-toolkit'
+    ],
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        mmcifprep=utils.mmcifprep:cli
+        mmcif2fmoinp=utils.mmcif2fmoinp:cli
+        gamout2tsv=utils.gamout2tsv:cli
+    ''',
+)
