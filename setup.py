19d3b30bc42993df2f726928f29786c1c5930675
Kazufumi Ohkawa <kerolinq@gmail.com>
2025-06-09 19:58:59 +0900

    Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FMOkit',
-    version='0.1',
+    version='0.2.0',
     py_modules=['FMOkit'],
     install_requires=[
         'click',
